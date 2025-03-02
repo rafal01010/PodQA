631ae71feabbc0634afc1bd7edfde10860359d5e
rafal01010 <dave.ramirez@eee.upd.edu.ph>
2025-03-02 16:44:43 +0800

    remove comments

--- yt-download/yt-dl.py
+++ yt-download/yt-dl.py
@@ -3,22 +3,21 @@ import os
 
 def download_mp3_from_channel(channel_url, num_videos, audio_quality='192'):
     ydl_opts = {
-        'format': 'bestaudio/best',  # Get the best audio quality available
+        'format': 'bestaudio/best',
         'postprocessors': [{
             'key': 'FFmpegExtractAudio',
             'preferredcodec': 'mp3',
-            'preferredquality': audio_quality,  # Set the audio quality
+            'preferredquality': audio_quality,
         }],
-        'noplaylist': False,  # Download all videos from the playlist or channel
-        'playlistend': None if num_videos == 'all' else num_videos,  # Limit the number of videos to download
-        'outtmpl': '%(title)s.%(ext)s',  # Output filename template
-        'ignoreerrors': True,  # Ignore errors to ensure the script continues
-        'nooverwrites': True,  # Do not overwrite existing files
-        'noprogress': True,  # Do not show the progress bar (optional)
+        'noplaylist': False,
+        'playlistend': None if num_videos == 'all' else num_videos,
+        'outtmpl': '%(title)s.%(ext)s',
+        'ignoreerrors': True,
+        'nooverwrites': True,
+        'noprogress': True,
     }
 
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        # Fetch the playlist information to get the list of video URLs
         info_dict = ydl.extract_info(channel_url, download=False)
         video_entries = info_dict.get('entries', [])
 
@@ -26,13 +25,10 @@ def download_mp3_from_channel(channel_url, num_videos, audio_quality='192'):
             video_title = entry.get('title', 'unknown')
             output_filename = f"{video_title}.mp3".replace("|","｜").replace("?","？")
             
-            
-            # Check if the output file already exists
             if os.path.exists(output_filename):
                 print(f"File '{output_filename}' already exists. Skipping...")
                 continue
             
-            # Download the video and perform post-processing
             ydl.download([entry['webpage_url']])
 
 if __name__ == "__main__":
